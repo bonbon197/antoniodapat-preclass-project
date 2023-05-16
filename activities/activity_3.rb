9d722bcfc91c472feaae6e6fd723fbcd0b304f41
bonbon <tony.dapat197@gmail.com>
2023-05-16 14:31:08 +0800

    Activity 3: Return the age gap from 1st to 2nd, 2nd to 3rd, 3rd to 4th, etc. in a given array

--- activities/activity_3.rb
+++ activities/activity_3.rb
@@ -2,3 +2,19 @@
 #  Write a function that takes an array of ages.
 #  The function will return an array of age differences between the 1st and 2nd age, 2nd and 3rd  .. so on and so forth.
 #  Print on console the result.
+
+def age_difference(array_of_ages)
+  age_gap = []
+
+  array_of_ages.each.with_index do |age, index|
+    unless index == array_of_ages.length - 1
+      gap = age - array_of_ages[index+1]
+      age_gap << gap
+    end
+  end
+
+  age_gap
+end
+
+p age_difference([1, 2, 3, 4, 5, 6, 7])
+p age_difference([0, 3, 6, 8, 9, 14, 23, 30])
