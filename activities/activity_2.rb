39a1130ec9faab4128111aacb3e457bcf21f4ea8
bonbon <tony.dapat197@gmail.com>
2023-05-16 14:10:20 +0800

    Activity 2: Returns a full name given the hashmap with the First_name, Middle_name, and Last_name keys

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,15 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def return_fullname(some_hash)
+  return "#{some_hash[:first_name]} #{some_hash[:middle_name]}, #{some_hash[:last_name]}"
+end
+
+name_hash = {
+  first_name: "Antonio",
+  middle_name: "Barangan",
+  last_name: "Dapat"
+}
+
+p return_fullname(name_hash)
